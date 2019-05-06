5ab1b6c8997e11a74045343fcd3ca81a44fce75b
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2019-05-06 13:18:17 -0400

    Read vehicle address from config file

--- src/main/java/com/shakhar/anki/intersectionhandler/Main.java
+++ src/main/java/com/shakhar/anki/intersectionhandler/Main.java
@@ -5,27 +5,37 @@ import de.adesso.anki.Vehicle;
 import de.adesso.anki.messages.SdkModeMessage;
 import de.adesso.anki.messages.SetSpeedMessage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        AnkiConnector connector = new AnkiConnector("localhost", 5000);
-        List<Vehicle> vehicles = connector.findVehicles();
-        if (vehicles.size() == 0) {
-            System.out.println("No Vehicles found.");
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
             return;
         }
-        System.out.println("Select Vehicle:");
-        for (int i = 0; i < vehicles.size(); i++) {
-            Vehicle vehicle = vehicles.get(i);
-            System.out.printf("%d) %s - %s %n", i+1, vehicle.getAddress(), vehicle.getAdvertisement());
+        String configVehicleAddress = properties.getProperty("anki.vehicle.address");
+        AnkiConnector connector = new AnkiConnector("localhost", 5000);
+        List<Vehicle> vehicles = connector.findVehicles();
+        Vehicle configVehicle = null;
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getAddress().equals(configVehicleAddress)) {
+                configVehicle = vehicle;
+                break;
+            }
         }
-        Scanner sc = new Scanner(System.in);
-        int selection = sc.nextInt();
-        Vehicle myVehicle = vehicles.get(selection - 1);
+        if (configVehicle == null) {
+            System.out.println("Vehicle Not Found.");
+            return;
+        }
+        Vehicle myVehicle = configVehicle;
         myVehicle.connect();
         myVehicle.sendMessage(new SdkModeMessage());
         myVehicle.sendMessage(new SetSpeedMessage(300, 300));
