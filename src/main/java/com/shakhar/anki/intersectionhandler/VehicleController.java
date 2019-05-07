9e342910444548659929234b7808a34ec085ea20
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2019-05-07 01:25:58 -0400

    Add functionality to broadcast and receive messages at intersection

--- src/main/java/com/shakhar/anki/intersectionhandler/VehicleController.java
+++ src/main/java/com/shakhar/anki/intersectionhandler/VehicleController.java
@@ -8,26 +8,48 @@ import de.adesso.anki.messages.SdkModeMessage;
 import de.adesso.anki.messages.SetSpeedMessage;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class VehicleController {
 
-    private final int ACCELERATION = 10000;
-    private final int INTERSECTION_PIECE_ID = 10;
+    private static final int ACCELERATION = 10000;
+    private static final int INTERSECTION_PIECE_ID = 10;
+    private static final int STOP_DURATION = 3000;
+
+    private static final String MULTICAST_ADDRESS = "225.225.225.225";
+    private static final int MULTICAST_PORT = 1234;
+
+    private static final byte ENTRY_BYTE = 1;
+    private static final byte EXIT_BYTE = 2;
 
     private String vehicleAddress;
     private int speed;
     private Vehicle vehicle;
 
+    private InetAddress groupAddress;
+    private MulticastSocket multicastSocket;
+    private LinkedList<InetAddress> list;
+
+    private Lock lock;
+    private Condition firstInList;
+
     public VehicleController(String vehicleAddress, int speed) throws IOException {
         this.vehicleAddress = vehicleAddress;
         this.speed = speed;
-        init();
+        groupAddress = InetAddress.getByName(MULTICAST_ADDRESS);
+        multicastSocket = new MulticastSocket(MULTICAST_PORT);
+        multicastSocket.joinGroup(groupAddress);
+        list = new LinkedList<>();
+        lock = new ReentrantLock();
+        firstInList = lock.newCondition();
+        initAnki();
     }
 
-    public void init() throws IOException {
+    public void initAnki() throws IOException {
         AnkiConnector connector = new AnkiConnector("localhost", 5000);
         List<Vehicle> vehicles = connector.findVehicles();
         for (Vehicle iterVehicle : vehicles) {
@@ -47,6 +69,10 @@ public class VehicleController {
         go();
         LocalizationPositionUpdateHandler liuh = new LocalizationPositionUpdateHandler();
         vehicle.addMessageListener(LocalizationPositionUpdateMessage.class, liuh);
+        new Thread(() -> {
+            while (true)
+                multicastReceive();
+        }).start();
     }
 
     private void go() {
@@ -57,19 +83,115 @@ public class VehicleController {
         vehicle.sendMessage(new SetSpeedMessage(0, ACCELERATION));
     }
 
+    private void onEnteringIntersection() {
+        stop();
+        long arrivalTime = System.currentTimeMillis();
+        broadcastEntry();
+        lock.lock();
+        try {
+            while (!isSelfAddress(list.get(0)))
+                firstInList.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        long duration = System.currentTimeMillis() - arrivalTime;
+        if (duration >= STOP_DURATION)
+            go();
+        else {
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    go();
+                }
+            }, duration);
+        }
+    }
+
+    private void onExitingIntersection() {
+        broadcastExit();
+    }
+
+    private void broadcastEntry() {
+        multicastSend(ENTRY_BYTE);
+    }
+
+    private void broadcastExit() {
+        multicastSend(EXIT_BYTE);
+    }
+
+    private void multicastSend(byte b) {
+        DatagramPacket packet = new DatagramPacket(new byte[b], 1, groupAddress, MULTICAST_PORT);
+        try {
+            multicastSocket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void multicastReceive() {
+        byte[] buffer = new byte[1];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        try {
+            multicastSocket.receive(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        switch (buffer[0]) {
+            case ENTRY_BYTE:
+                onEntryReceive(packet.getAddress());
+                break;
+            case EXIT_BYTE:
+                onExitReceive(packet.getAddress());
+        }
+    }
+
+    private void onEntryReceive(InetAddress sender) {
+        lock.lock();
+        try {
+            list.add(sender);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void onExitReceive(InetAddress sender) {
+        lock.lock();
+        try {
+            list.remove(sender);
+            firstInList.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private static boolean isSelfAddress(InetAddress address) {
+        Enumeration<NetworkInterface> interfaces = null;
+        try {
+            interfaces = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        while(interfaces.hasMoreElements()) {
+            Enumeration<InetAddress> addresses = interfaces.nextElement().getInetAddresses();
+            while(addresses.hasMoreElements())
+                if(addresses.nextElement().equals(address))
+                    return true;
+        }
+        return false;
+    }
+
     private class LocalizationPositionUpdateHandler implements MessageListener<LocalizationPositionUpdateMessage> {
 
         int lastRoadPiece;
         @Override
         public void messageReceived(LocalizationPositionUpdateMessage message) {
             if (message.getRoadPieceId() == INTERSECTION_PIECE_ID && lastRoadPiece != INTERSECTION_PIECE_ID) {
-                stop();
-                new Timer().schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        go();
-                    }
-                }, 3000);
+                onEnteringIntersection();
+            } else if (message.getRoadPieceId() != INTERSECTION_PIECE_ID && lastRoadPiece == INTERSECTION_PIECE_ID) {
+                onExitingIntersection();
             }
             lastRoadPiece = message.getRoadPieceId();
         }
